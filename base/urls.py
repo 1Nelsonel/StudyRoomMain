be431787df72429bb26f4601e2e90c6a7959ffde
1Nelsonel <nelsonmokoro37@gmail.com>
2022-08-07 18:51:39 +0300

    success

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('community/', views.community, name='community'),
+    path('contact/', views.contact, name='contact'),
+    path('course/', views.course, name='course'),
 ]
--- base/views.py
+++ base/views.py
@@ -6,3 +6,23 @@ from django.contrib import messages
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'base/about.html', context)
+
+def course(request):
+    context = {}
+    return render(request,'base/course.html', context)
+
+def community(request):
+    context = {}
+    return render(request, 'base/community.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'base/contact.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'base/blog.html', context)
